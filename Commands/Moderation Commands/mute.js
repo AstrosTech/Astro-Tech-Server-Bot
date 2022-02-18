0989599a9ca33c58e11b844bd8b47b8941ac1f4a
Sebastian Lample <SebastianLample@gmail.com>
2022-02-18 15:45:06 -0800

    Give Muted role to newly muted user

--- Commands/Moderation Commands/mute.js
+++ Commands/Moderation Commands/mute.js
@@ -6,19 +6,23 @@ module.exports.run = async (bot, message, args) => {
     let MuteMember = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     if(!MuteMember) return functions.InsufficientUsage(bot, message.guild, exports.help, message.author, message.channel)
 
-    let MuteCheck = MuteDatabase.findOne({ UserID: MuteMember.id, Active: true})
+    let MuteCheck = await MuteDatabase.findOne({ UserID: MuteMember.id, Active: true })
     if(MuteCheck) return message.channel.send({ embeds: [functions.EmbedGenerator(bot, config.GeneralEmbeds.Errors, [`{Error}---${MuteMember.toString()} is already muted.`], message.author)] })
 
+    let MuteRole = message.guild.roles.cache.find(role => role.id == config.MutedRoleID)
+    if(!MuteRole) return message.channel.send({ embeds: [functions.EmbedGenerator(bot, config.GeneralEmbeds.Errors, [`{Error}---MutedRoleID is not set correctly in the configuration.`], message.author)] })
     
+    MuteMember.roles.add(config.MutedRoleID)
+    MuteDatabase.create({ UserID: MuteMember.user.id })
 }
 
 const CommandHelp = require('../../Configuration/YML').LoadCommandConfiguration();
 module.exports.help = {
-    name: CommandHelp.ModerationCommands.Lock.name,
-    description: CommandHelp.ModerationCommands.Lock.description,
-    usage: CommandHelp.ModerationCommands.Lock.usage,
-    aliases: CommandHelp.ModerationCommands.Lock.aliases,
-    permissions: CommandHelp.ModerationCommands.Lock.permissions,
-    cooldown: CommandHelp.ModerationCommands.Lock.cooldown,
-    enabled: CommandHelp.ModerationCommands.Lock.enabled
+    name: CommandHelp.ModerationCommands.Mute.name,
+    description: CommandHelp.ModerationCommands.Mute.description,
+    usage: CommandHelp.ModerationCommands.Mute.usage,
+    aliases: CommandHelp.ModerationCommands.Mute.aliases,
+    permissions: CommandHelp.ModerationCommands.Mute.permissions,
+    cooldown: CommandHelp.ModerationCommands.Mute.cooldown,
+    enabled: CommandHelp.ModerationCommands.Mute.enabled
 }
